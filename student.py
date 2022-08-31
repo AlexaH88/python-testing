a8971e4582e04777144afb36ea1470572e38ea1c
Alexa Hendry <alexahendry88@gmail.com>
2022-08-31 13:28:54 +0000

    Complete student testing module

--- student.py
+++ student.py
@@ -1,4 +1,5 @@
 from datetime import date, timedelta
+import requests
 
 
 class Student:
@@ -15,3 +16,25 @@ class Student:
     @property
     def full_name(self):
         return f"{self._first_name} {self._last_name}"
+
+
+    def alert_santa(self):
+        self.naughty_list = True
+
+
+    @property
+    def email(self):
+        return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
+
+
+    def apply_extension(self, days):
+        self.end_date = self.end_date + timedelta(days=days)
+
+
+    def course_schedule(self):
+        response = requests.get(f"http://company.com/course-schedule/{self._last_name}/{self._first_name}")
+
+        if response.ok:
+            return response.text
+        else:
+            return "Something went wrong with the request!"
--- test_student.py
+++ test_student.py
@@ -1,13 +1,69 @@
 import unittest
 from student import Student
+from datetime import timedelta
+from unittest.mock import patch
 
 
 class TestStudent(unittest.TestCase):
 
-    def test_full_name(self):
-        student = Student('John', 'Doe')
+    @classmethod
+    def setUpClass(cls):
+        print('setUpClass')
 
-        self.assertEqual(student.full_name, 'John Doe')
+
+    @classmethod
+    def tearDownClass(cls):
+        print('tearDownClass')
+
+
+    def setUp(self):
+        print('setUp')
+        self.student = Student('John', 'Doe')
+
+
+    def tearDown(self):
+        print('tearDown')
+
+
+    def test_full_name(self):
+        print('test_full_name')
+        self.assertEqual(self.student.full_name, 'John Doe')
+
+
+    def test_alert_santa(self):
+        print('test_alert_santa')
+        self.student.alert_santa()
+        self.assertTrue(self.student.naughty_list)
+
+
+    def test_email(self):
+        print('test_email')
+        self.assertEqual(self.student.email, 'john.doe@email.com')
+
+
+    def test_apply_extension(self):
+        print('test_apply_extension')
+        old_end_date = self.student.end_date
+        self.student.apply_extension(5)
+        self.assertEqual(self.student.end_date, old_end_date + timedelta(5))
+
+
+    def test_course_schedule_success(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text = "Success"
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Success")
+
+
+    def test_course_schedule_failed(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = False
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Something went wrong with the request!")
+
 
 if __name__ == '__main__':
     unittest.main()
